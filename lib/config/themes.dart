504b41feb5ec01ec766209335bdb45940f8f9cf4
Mark <markjeraldc@gmail.com>
2022-04-25 21:16:41 +0800

    Update themes.dart

--- lib/config/themes.dart
+++ lib/config/themes.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 ThemeData themeBlue = ThemeData(
-  primarySwatch: Colors.blueGrey,
-  primaryColor: Colors.red,
-  // canvasCsolor: Colors.grey.shade100,
-  cardColor: Colors.white,
-  shadowColor: Colors.blueAccent,
-  visualDensity: VisualDensity.comfortable,
-);
+    primarySwatch: Colors.blueGrey,
+    primaryColor: Colors.red,
+    canvasColor: Colors.grey.shade100,
+    cardColor: Colors.white,
+    shadowColor: Colors.blueAccent,
+    visualDensity: VisualDensity.comfortable,
+    fontFamily: "Times");
