b5e31f30daf9590b599f8f315d0aa150d09a9163
Mark <markjeraldc@gmail.com>
2022-04-25 21:14:31 +0800

    Fix: Fetch type data

--- lib/service/provider/github_api_service.dart
+++ lib/service/provider/github_api_service.dart
@@ -1,35 +1,45 @@
-import 'dart:convert' as convert;
+import 'dart:convert';
+import 'package:github_flutter_bloc/modules/model/github_repositories_model.dart';
+import 'package:github_flutter_bloc/modules/model/search_result_error.dart';
+
 import 'package:http/http.dart' as http;
 
 class GitHubApiService {
   final baseApi = 'https://api.github.com';
 
-  Future findUserRepositories(String name) async {
-    var jsonResponse;
+  Future<List<GitHubRepositoriesModel>> findUserRepositories(
+      String name) async {
     var url = Uri.parse('$baseApi/users/$name/repos');
 
-    var response = await http.get(url);
+    final response = await http.get(url);
+    final result = json.decode(response.body);
 
     if (response.statusCode == 200) {
-      return jsonResponse = convert.jsonDecode(response.body) as List;
+      List<GitHubRepositoriesModel> _lst =
+          gitHubRepositoriesModelFromJson(response.body);
+      return _lst;
+      // return RepoSearch.fromJson(result);
     } else {
-      return jsonResponse = response.statusCode;
+      throw SearchResultError.fromJson(result);
     }
   }
 
-  Future listGithubUser({String? uName, String? dLink}) async {
-    uName = uName == null ? '' : uName = '/$uName';
-    dLink = dLink == null ? '' : dLink = '/$dLink';
+  // Future listGithubUser({String? uName, String? dataLink}) async {
+  //   uName = uName == null ? '' : uName = '/$uName';
+  //   dataLink = dataLink == null ? '' : dataLink = '/$dataLink';
 
-    var jsonResponse;
-    var url = Uri.parse('$baseApi/users/$uName$dLink');
+  //   var jsonResponse;
+  //   var url = Uri.parse('$baseApi/users/$uName$dataLink');
 
-    var response = await http.get(url);
+  //   final response = await http.get(url);
+  //   final result = json.decode(response.body);
 
-    if (response.statusCode == 200) {
-      return jsonResponse = convert.jsonDecode(response.body) as List;
-    } else {
-      return jsonResponse = response.statusCode;
-    }
-  }
+  //   if (response.statusCode == 200) {
+  //     // return jsonResponse = convert.jsonDecode(response.body) as List;
+  //     return GitHubRepositoriesModel.fromJson(result);
+  //   } else {
+  //     // return jsonResponse = response.statusCode;
+  //     throw SearchResultError.fromJson(result);
+  //   }
+  // }
 }
