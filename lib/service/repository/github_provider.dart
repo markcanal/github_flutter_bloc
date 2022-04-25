39ced11981054f60834e696748ede655503ed69d
Mark <markjeraldc@gmail.com>
2022-04-25 21:13:32 +0800

    Delete github_provider.dart

--- lib/service/repository/github_provider.dart
+++
@@ -1,7 +0,0 @@
-import 'package:github_flutter_bloc/service/provider/github_api_service.dart';
-
-class GithubProvider {
-  GithubProvider({required this.gitHubApiService});
-
-  final GitHubApiService gitHubApiService;
-}
