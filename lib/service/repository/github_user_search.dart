79a5036bc12a83faa06f41013d02c5e25d04bb91
Mark <markjeraldc@gmail.com>
2022-04-25 21:13:24 +0800

    Create github_user_search.dart

---
+++ lib/service/repository/github_user_search.dart
@@ -0,0 +1,13 @@
+import 'package:github_flutter_bloc/modules/model/github_repositories_model.dart';
+import 'package:github_flutter_bloc/service/provider/github_api_service.dart';
+
+class GithubRepositorySearch {
+  GithubRepositorySearch({required this.gitHubApiService});
+
+  final GitHubApiService gitHubApiService;
+
+  Future<List<GitHubRepositoriesModel>> getSearch(String str) async {
+    final result = gitHubApiService.findUserRepositories(str);
+    return result;
+  }
+}
