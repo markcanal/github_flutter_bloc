137c3eb32e32061df871d9e92adb217645fbba5b
Mark <markjeraldc@gmail.com>
2022-04-26 11:28:42 +0800

    refactor:themes

--- lib/config/themes.dart
+++ lib/config/themes.dart
@@ -3,8 +3,14 @@ import 'package:flutter/material.dart';
 ThemeData themeBlue = ThemeData(
     primarySwatch: Colors.blueGrey,
     primaryColor: Colors.red,
-    canvasColor: Colors.grey.shade100,
+    canvasColor: Colors.white,
     cardColor: Colors.white,
     shadowColor: Colors.blueAccent,
     visualDensity: VisualDensity.comfortable,
+    tabBarTheme: const TabBarTheme(
+        labelColor: Colors.black,
+        labelPadding: EdgeInsets.all(5),
+        unselectedLabelColor: Colors.blueGrey,
+        unselectedLabelStyle:
+            TextStyle(fontSize: 12, fontWeight: FontWeight.bold)),
     fontFamily: "Times");
