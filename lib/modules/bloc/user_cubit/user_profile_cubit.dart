601474b4c235e9842fcdd44ef5c903f8d4a089bb
Mark <markjeraldc@gmail.com>
2022-04-26 22:58:09 +0800

    remove

--- lib/modules/bloc/user_cubit/user_profile_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
-
-part 'user_profile_state.dart';
-
-class UserProfileCubit extends Cubit<UserProfileState> {
-  UserProfileCubit() : super(UserProfileInitial());
-}
